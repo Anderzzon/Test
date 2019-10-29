215712aa78079ba805c0f8676c3ce5bf041558be
Erik <erik.anderzzon@gmail.com>
2019-10-29 16:17:08 +0100

    TESTING

--- places.js
+++ places.js
@@ -55,7 +55,7 @@ function staticLoadPlaces() {
 // getting places from REST APIs
 function dynamicLoadPlaces(position) {
     let params = {
-        radius: 300,    // search places not farther than this value (in meters)
+        radius: 500,    // search places not farther than this value (in meters)
         clientId: 'HZIJGI4COHQ4AI45QXKCDFJWFJ1SFHYDFCCWKPIJDWHLVQVZ',
         clientSecret: '',
         version: '20300101',    // foursquare versioning, required but unuseful for this demo
