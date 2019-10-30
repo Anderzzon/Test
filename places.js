dea4f004ad62c1c99f2f7b5097c6ea7de82298d5
Erik <erik.anderzzon@gmail.com>
2019-10-30 13:05:11 +0100

    Update places.js
    Removede code that was used for dynamic Foursquare functions

--- places.js
+++ places.js
@@ -1,38 +1,11 @@
 window.onload = () => {
-    // if you want to dynamically add places, de-comment following line
-    // let method = 'dynamic';
-
-    // if you want to statically add places, de-comment following line (Done)
      let method = 'static';
 
-    if (method === 'static') {
         // setTimeout is a temporary fix
         setTimeout(() => {
             let places = staticLoadPlaces();
             renderPlaces(places);
         }, 3000);
-    }
-/* This if-statment should be used if a dynamic method should be used
-    if (method !== 'static') { 
-
-        // first get current user location
-        return navigator.geolocation.getCurrentPosition(function (position) {
-
-            // than use it to load from remote APIs some places nearby
-            dynamicLoadPlaces(position.coords)
-                .then((places) => {
-                    renderPlaces(places);
-                })
-        },
-            (err) => console.error('Error in retrieving position', err),
-            {
-                enableHighAccuracy: true,
-                maximumAge: 0,
-                timeout: 27000,
-            }
-        );
-    }
-    */
 };
 function staticLoadPlaces() {
     //Arrey with the places
@@ -60,38 +33,6 @@ function staticLoadPlaces() {
         }
     ];
 }
-
-// getting places from REST APIs - //This funktion is not used
-function dynamicLoadPlaces(position) {
-    let params = {
-        radius: 300,    // search places not farther than this value (in meters)
-        clientId: 'HZIJGI4COHQ4AI45QXKCDFJWFJ1SFHYDFCCWKPIJDWHLVQVZ',
-        clientSecret: '',
-        version: '20300101',    // foursquare versioning, required but unuseful for this demo
-    };
-
-    // CORS Proxy to avoid CORS problems
-    let corsProxy = 'https://cors-anywhere.herokuapp.com/';
-
-    // Foursquare API
-    let endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
-        &ll=${position.latitude},${position.longitude}
-        &radius=${params.radius}
-        &client_id=${params.clientId}
-        &client_secret=${params.clientSecret}
-        &limit=15
-        &v=${params.version}`;
-    return fetch(endpoint)
-        .then((res) => {
-            return res.json()
-                .then((resp) => {
-                    return resp.response.venues;
-                })
-        })
-        .catch((err) => {
-            console.error('Error with places API', err);
-        })
-};
 
 function renderPlaces(places) {
     let scene = document.querySelector('a-scene');
@@ -140,10 +81,6 @@ function renderPlaces(places) {
 }
 
 window.onload = () => {
-    // if you want to dynamically add places, de-comment following line
-    // let method = 'dynamic';
-
-    // if you want to statically add places, de-comment following line (Done)
     let method = 'static';
 
     if (method === 'static') {
@@ -152,25 +89,5 @@ window.onload = () => {
             let places = staticLoadPlaces();
             renderPlaces(places);
         }, 3000);
-    }
-
-    if (method !== 'static') { //This if-statment is not used right now
-
-        // first get current user location
-        return navigator.geolocation.getCurrentPosition(function (position) {
-
-            // than use it to load from remote APIs some places nearby
-            dynamicLoadPlaces(position.coords)
-                .then((places) => {
-                    renderPlaces(places);
-                })
-        },
-            (err) => console.error('Error in retrieving position', err),
-            {
-                enableHighAccuracy: true,
-                maximumAge: 0,
-                timeout: 27000,
-            }
-        );
     }
 };
